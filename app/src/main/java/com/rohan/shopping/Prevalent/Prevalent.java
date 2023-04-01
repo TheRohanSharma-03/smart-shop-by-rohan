dac799372df003b121167d970bcbea1e4a77c2f1
Rohan Sharma <rsherors123@gmail.com>
2023-04-01 05:28:55 -0700

    Initial commit

---
+++ app/src/main/java/com/rohan/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.rohan.shopping.Prevalent;
+
+import com.rohan.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
